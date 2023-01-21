da661645d4520a318bef340daba6301184b41f6c
Ivan Muratov <binakot@gmail.com>
2023-01-21 21:35:43 +0300

    API with file upload

--- waliot/app.py
+++ waliot/app.py
@@ -2,10 +2,11 @@ import os
 import sys
 import traceback
 
+import cv2
+import numpy as np
 import ujson
 from flask import Flask
 from flask import request
-from flask_wtf.csrf import CSRFProtect
 
 from nomeroff_net import __version__
 from nomeroff_net import pipeline
@@ -22,9 +23,8 @@ number_plate_detection_and_reading = pipeline("multiline_number_plate_detection_
                                                   },
                                               },
                                               default_label="ru",
-                                              image_loader="opencv")
+                                              image_loader=None)
 
-csrf = CSRFProtect()
 app = Flask(__name__)
 
 
@@ -51,8 +51,29 @@ def detect():
         return ujson.dumps(dict(error=str(e), img_path=img_path))
 
 
+@app.route('/magic', methods=['POST'])
+def magic():
+    file = request.files['file'].read()
+
+    try:
+        (images, images_bboxs,
+         images_points, images_zones, region_ids,
+         region_names, count_lines,
+         confidences, texts) = fileToImage(file)
+
+        return ujson.dumps(dict(res=texts))
+    except Exception as e:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_tb)
+        return ujson.dumps(dict(error=str(e)))
+
+
+def fileToImage(file):
+    image = cv2.imdecode(np.frombuffer(file, dtype=np.uint8), 1)
+    return unzip(number_plate_detection_and_reading([image]))
+
+
 if __name__ == '__main__':
-    csrf.init_app(app)
     app.run(debug=False,
             port=os.environ.get("PORT", 8888),
             host='0.0.0.0',
